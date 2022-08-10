f3eb421cc2d332bb48b49f06e6fee7a0ff9e3159
Talha <taldemirtas@gmail.com>
2022-08-10 22:25:49 +0300

    Details

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,19 +1,20 @@
 import { useParams } from 'react-router-dom';
 import { GetBlog } from '../helpers/firebase';
+import { useNavigate } from 'react-router-dom';
 
 const Details = ({ imgurl, title, content }) => {
   const { blogList } = GetBlog();
-  console.log(blogList);
+  const navigate = useNavigate();
   const { id } = useParams();
+  const details = blogList?.find?.(blog=> blog.id === id)
+  // console.log(blogList.find(blog=> blog.id === id))
 
   return (
-    <div className="card">
-      <p className="card-text">{id}</p>
-      <img className="card-img-top" src={imgurl} loading="lazy" alt="mgi" />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{content}</p>
-      </div>
+    <div className="d-flex flex-column justify-content-center p-5">
+      <img className='detail-img img-fluid' src={details?.imgurl} loading="lazy" alt="mgi" />
+        <h5>{details?.title}</h5>
+        <p>{details?.content}</p>
+        <button className='btn btn-primary' onClick={() => {navigate(-1)}}>Main Page</button>
     </div>
   );
 };
