08052fb4ecf68ff3a253fa979b8a47c5d5b9bacf
Talha <taldemirtas@gmail.com>
2022-08-10 21:07:59 +0300

    NewBlog fix

--- src/pages/NewBlog.jsx
+++ src/pages/NewBlog.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import BlogForm from '../components/BlogForm';
 import { AddBlog } from '../helpers/firebase';
+import { toastSuccessNotify } from '../helpers/toastNotify';
 
 const initialValues = { title: '', imgurl: '', content: '' };
 
 function NewBlog() {
-  const [blog, setBlog] = useState(initialValues);
 
+  const [blog, setBlog] = useState(initialValues);
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
     AddBlog(blog);
+    toastSuccessNotify('New blog added');
+    navigate('/')
   };
 
   return (
