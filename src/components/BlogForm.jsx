e1594b4bbe2eb9181148848fbf5dc32a9f6241b7
Talha <taldemirtas@gmail.com>
2022-08-10 19:37:47 +0300

    BlogForm fixed

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,5 +1,3 @@
-import nBlog from '../assets/blok.png';
-
 const BlogForm = ({ blog, setBlog, handleSubmit }) => {
   const handleChange = (e) => {
     e.preventDefault();
@@ -9,64 +7,52 @@ const BlogForm = ({ blog, setBlog, handleSubmit }) => {
   };
 
   return (
-    <div>
-      <div className="card bg-dark text-white">
-        <img
-          src={'https://picsum.photos/800/800'}
-          className="card-img"
-          alt="samp"
-        />
-        <div className="card-img-overlay register-form p-5 w-75">
-          <div className="d-flex">
-            <h1 className="form-title display-3">Add_New_Blog</h1>
-            <img src={nBlog} alt="blokimg" width="75px" />
-          </div>
-          <form id="addblog" onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label className="form-label">Title</label>
-              <input
-                type="text"
-                className="form-control"
-                id="title"
-                placeholder="Enter a title"
-                required
-                value={blog.title}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Image URL</label>
-              <input
-                type="text"
-                className="form-control"
-                id="imgurl"
-                placeholder="Enter your image URL"
-                required
-                value={blog.imgurl}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Content</label>
-              <textarea
-                type="text"
-                className="form-control"
-                rows="6"
-                id="content"
-                placeholder="Enter your content"
-                required
-                value={blog.content}
-                onChange={handleChange}
-              />
-            </div>
-            <input
-              type="submit"
-              className="btn btn-primary form-control"
-              value="Add"
-            />
-          </form>
+    <div className="d-flex px-5">
+      <form onSubmit={handleSubmit}>
+        <h1 className="form-title display-4">Add New Blog</h1>
+        <div className="mb-3">
+          <label className="form-label">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Enter a title"
+            required
+            value={blog.title}
+            onChange={handleChange}
+          />
         </div>
-      </div>
+        <div className="mb-3">
+          <label className="form-label">Image URL</label>
+          <input
+            type="text"
+            className="form-control"
+            id="imgurl"
+            placeholder="Enter your image URL"
+            required
+            value={blog.imgurl}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Content</label>
+          <textarea
+            type="text"
+            className="form-control"
+            rows="6"
+            id="content"
+            placeholder="Enter your content"
+            required
+            value={blog.content}
+            onChange={handleChange}
+          />
+        </div>
+        <input
+          type="submit"
+          className="btn btn-primary form-control"
+          value="Add"
+        />
+      </form>
     </div>
   );
 };
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,15 @@
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useContext(AuthContext);
+  const navName = currentUser.displayName;
+
+  return <div>
+  <div className='d-flex p-5 justify-content-center'>
+  Merhaba {navName}, blog sitesine hoş geldin
+  </div>
+</div>
 };
 
 export default Profile;
